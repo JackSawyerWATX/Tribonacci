9d39620d413534e819a895b4795be75b81a672c2
Jonathan <jacksawyerwatx@users.noreply.github.com>
2025-01-15 10:48:25 -0600

    Updated Console.logs

--- script.js
+++ script.js
@@ -16,4 +16,16 @@ var tribonacci = function(n) {
     return t2;
 };
 
+console.log(tribonacci(4)); // Output: 4
+console.log(tribonacci(6)); // Output: 13
+console.log(tribonacci(10)); // Output: 149
+console.log(tribonacci(15)); // Output: 1705
+console.log(tribonacci(20)); // Output: 35890
 console.log(tribonacci(23)); // Output: 410744
+console.log(tribonacci(25)); // Output: 1389537
+console.log(tribonacci(37)); // Output: 2082876103
+console.log(tribonacci(50)); // Output: 1132436852
+console.log(tribonacci(75)); // Output: 9070065305162309856
+console.log(tribonacci(100)); // Output: 180396380815100901214157639
+console.log(tribonacci(1000)); // Output: 70330367711422815821835254877183549770181269836358732742604905087154537118196933579742249494562611733487750449241765991088186363265450223647106012053374121273867339111198139373125598767690091902245245323403501
+console.log(tribonacci(10000)); // Output: Infinity
